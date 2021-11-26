86e9caccabd642970107e42b52715ac246338380
shu Lin <bbqlinshu@gmail.com>
2021-11-26 15:35:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(record){
+    const winRecord = record.find((current, index, array) => {
+        return current.result === "W";
+    })
+    if (winRecord !== undefined){
+        return winRecord.year;
+    }
+    return undefined;
+}
 
